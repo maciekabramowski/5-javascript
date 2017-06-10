e0101cab0635c525a655df428c5a667d23c1e5c0
Maciej Abramowski <m.abramowski@wp.pl>
2017-06-10 11:36:09 +0200

    fin

--- js/5-objects-as-variable.js.js
+++ js/5-objects-as-variable.js.js
@@ -1,5 +1,17 @@
 'use strict';
 
+/*
 
+Obiekty jako zmienne
 
+*/
 
+var student = {
+    imie: "Kaja",
+    wzrost: 152,
+    wyswietlDane: function() {console.log(this.imie)}
+}
+
+student.wyswietlDane();
+
+console.log(student);
